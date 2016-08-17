fff854e800d45d5a3934e0a8b493e4fe72e3d319
Adam Elliot <adam@adamelliot.com>
2016-08-16 22:16:37 -0700

    Moved palettes to cpp.

--- src/palette.h
+++ src/palette.h
@@ -198,49 +198,41 @@ typedef TPalette<TRGB, uint8_t> CRGBPalette;
 typedef TPalette<TRGB, uint8_t> RGBuPalette;
 typedef TPalette<TRGB, uint8_t> RGBAuPalette;
 
-static ColorPaletteData PROGMEM SOLID_RED 						= { HTML::Red };
-static ColorPaletteData PROGMEM SOLID_GREEN						= { HTML::Lime };
-static ColorPaletteData PROGMEM SOLID_BLUE						= { HTML::Blue };
-static ColorPaletteData PROGMEM SOLID_AQUA						= { HTML::Aqua };
-static ColorPaletteData PROGMEM SOLID_AZURE						= { HTML::Azure };
-
-static ColorPaletteData PROGMEM HSV_GRADIENT					= { HTML::Red, 0xffff00, HTML::Lime, 0x00ffff, HTML::Blue, 0xff00ff, HTML::Red };
-static ColorPaletteData PROGMEM RAINBOW_GRADIENT 				= { HTML::Red, HTML::Lime, HTML::Blue, HTML::Red };
-
-static ColorPaletteData PROGMEM BLUE_GREEN_GRADIENT 			= { HTML::Blue, RGB_HEX(0, 255, 64), HTML::Blue };
-static ColorPaletteData PROGMEM GREEN_GRADIENT 					= { HTML::Lime, RGB_HEX(0, 32, 0), HTML::Lime };
-static ColorPaletteData PROGMEM LIME_GRADIENT 					= { HTML::Lime, HTML::Green, HTML::Lime };
-static ColorPaletteData PROGMEM YELLOW_GRADIENT 				= { RGB_HEX(255, 191, 63), RGB_HEX(63, 47, 15), RGB_HEX(255, 191, 63) };
-static ColorPaletteData PROGMEM WHITE_GRADIENT 					= { HTML::White, HTML::Black, HTML::White };
-
-static ColorPaletteData PROGMEM RED_GREEN_GRADIENT 				= { HTML::Red, HTML::Green };
-static ColorPaletteData PROGMEM GREEN_BLUE_GRADIENT 			= { RGB_HEX(0, 255, 64), RGB_HEX(0, 0, 255), RGB_HEX(0, 255, 64) };
-static ColorPaletteData PROGMEM RED_ORGANGE_GRADIENT 			= { RGB_HEX(128, 128, 128), RGB_HEX(128, 31, 0), RGB_HEX(128, 95, 0), RGB_HEX(128, 31, 0), RGB_HEX(128, 128, 128) };
-static ColorPaletteData PROGMEM BLUE_WHITE_GRADIENT 			= { RGB_HEX(0, 31, 255), RGB_HEX(255, 255, 255), RGB_HEX(0, 31, 255) };
-static ColorPaletteData PROGMEM BLUE_WHITISH_GRADIENT			= { RGB_HEX(0, 31, 255), RGB_HEX(191, 191, 191), RGB_HEX(0, 31, 255) };
-
-static ColorPaletteData PROGMEM BLUE_WHITE_YELLOW_GRADIENT 		= { RGB_HEX(0, 95, 255), RGB_HEX(255, 255, 0), HTML::White, RGB_HEX(0, 31, 255) };
-
-static ColorPaletteData PROGMEM BLUE_YELLOW_BLACK_GRADIENT 		= { RGB_HEX(0, 95, 255), RGB_HEX(255, 255, 0), HTML::White, HTML::Black, RGB_HEX(0, 31, 255) };
-static ColorPaletteData PROGMEM RED_WHITE_BLACK_GRADIENT 		= { RGB_HEX(255, 0, 31), RGB_HEX(255, 0, 0), HTML::White, HTML::Black, RGB_HEX(255, 0, 31) };
-
-static ColorPaletteData PROGMEM RED_WHITE_GRADIENT 				= { RGB_HEX(255, 31, 0), RGB_HEX(255, 0, 0), RGB_HEX(255, 255, 255), RGB_HEX(255, 31, 0) };
-static ColorPaletteData PROGMEM GREEN_WHITE_GRADIENT 			= { RGB_HEX(0, 255, 31), RGB_HEX(0, 255, 0), RGB_HEX(255, 255, 255), RGB_HEX(0, 255, 31) };
-static ColorPaletteData PROGMEM RED_WHITISH_GRADIENT 			= { RGB_HEX(255, 31, 0), RGB_HEX(255, 0, 0), RGB_HEX(191, 191, 191), RGB_HEX(255, 31, 0) };
-
-static ColorPaletteData PROGMEM CYAN_PINK_GRADIENT 				= { RGB_HEX(0, 255, 255), RGB_HEX(255, 0, 96), RGB_HEX(0, 255, 255) };
-static ColorPaletteData PROGMEM BLUE_YELLOW_GRADIENT 			= { RGB_HEX(0, 95, 255), RGB_HEX(255, 255, 0), RGB_HEX(0, 31, 255) };
-static ColorPaletteData PROGMEM GREEN_YELLOW_GRADIENT 			= { RGB_HEX(31, 255, 31), RGB_HEX(255, 191, 63), RGB_HEX(31, 255, 31) };
-static ColorPaletteData PROGMEM RAINBOW_BLACK_GRADIENT 			= { HTML::Red, HTML::Green, HTML::Blue, HTML::Black, HTML::Red };
-
-static ColorPaletteData PROGMEM PINK_PURPLE_GRADIENT			= { RGB_HEX(255, 0, 128), RGB_HEX(96, 0, 192), RGB_HEX(255, 0, 128) };
-static ColorPaletteData PROGMEM PINK_PURPLE_BLACK_GRADIENT		= { RGB_HEX(255, 0, 128), RGB_HEX(96, 0, 192), HTML::Black, HTML::Black, RGB_HEX(255, 0, 128) };
-static ColorPaletteData PROGMEM CYAN_PINK_BLACK_GRADIENT		= { RGB_HEX(0, 255, 192), HTML::Black, RGB_HEX(255, 16, 64), HTML::Black, RGB_HEX(0, 255, 192) };
-static ColorPaletteData PROGMEM GREEN_BLUE_BLACK_GRADIENT		= { RGB_HEX(0, 255, 64), RGB_HEX(0, 0, 255), HTML::Black, HTML::Black, RGB_HEX(0, 255, 64) };
-static ColorPaletteData PROGMEM RED_ORGANGE_BLACK_GRADIENT		= { RGB_HEX(255, 31, 0), RGB_HEX(255, 191, 0), HTML::Black, HTML::Black, RGB_HEX(255, 31, 0) };
-static ColorPaletteData PROGMEM BLUE_WHITE_BLACK_GRADIENT		= { RGB_HEX(0, 31, 25), RGB_HEX(255, 255, 255), HTML::White, HTML::Black, RGB_HEX(0, 31, 255) };
-static ColorPaletteData PROGMEM BLUE_YELLOW_BLACK_HARD_GRADIENT = { RGB_HEX(0, 0, 0), RGB_HEX(16, 16, 0), RGB_HEX(255, 255, 0), RGB_HEX(0, 95, 255) };
-static ColorPaletteData PROGMEM WHITE_SOLID_GRADIENT			= { RGB_HEX(64, 64, 64), RGB_HEX(64, 64, 64) };
+extern const ColorPaletteData PROGMEM SOLID_RED;
+extern const ColorPaletteData PROGMEM SOLID_GREEN;
+extern const ColorPaletteData PROGMEM SOLID_BLUE;
+extern const ColorPaletteData PROGMEM SOLID_AQUA;
+extern const ColorPaletteData PROGMEM SOLID_AZURE;
+extern const ColorPaletteData PROGMEM HSV_GRADIENT;
+extern const ColorPaletteData PROGMEM RAINBOW_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_GREEN_GRADIENT;
+extern const ColorPaletteData PROGMEM GREEN_GRADIENT;
+extern const ColorPaletteData PROGMEM LIME_GRADIENT;
+extern const ColorPaletteData PROGMEM YELLOW_GRADIENT;
+extern const ColorPaletteData PROGMEM WHITE_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_GREEN_GRADIENT;
+extern const ColorPaletteData PROGMEM GREEN_BLUE_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_ORGANGE_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_WHITE_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_WHITISH_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_WHITE_YELLOW_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_YELLOW_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_WHITE_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_WHITE_GRADIENT;
+extern const ColorPaletteData PROGMEM GREEN_WHITE_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_WHITISH_GRADIENT;
+extern const ColorPaletteData PROGMEM CYAN_PINK_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_YELLOW_GRADIENT;
+extern const ColorPaletteData PROGMEM GREEN_YELLOW_GRADIENT;
+extern const ColorPaletteData PROGMEM RAINBOW_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM PINK_PURPLE_GRADIENT;
+extern const ColorPaletteData PROGMEM PINK_PURPLE_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM CYAN_PINK_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM GREEN_BLUE_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM RED_ORGANGE_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_WHITE_BLACK_GRADIENT;
+extern const ColorPaletteData PROGMEM BLUE_YELLOW_BLACK_HARD_GRADIENT;
+extern const ColorPaletteData PROGMEM WHITE_SOLID_GRADIENT;
 
 /* --------------- Swatch Manager --------------- */ 
 
