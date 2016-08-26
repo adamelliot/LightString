cce253d42a14675eff71423c70524e83f5a0f4d5
Adam Elliot <adam@adamelliot.com>
2016-08-26 12:36:30 -0700

    Namespaced helpers.

--- src/utils.h
+++ src/utils.h
@@ -22,12 +22,14 @@ long random(long min, long max);
 
 #endif
 
+namespace LightString {
 template <typename T>
 T map(T x, T in_min, T in_max, T out_min, T out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+};
 
-#define mapf map<float>
+#define mapf LightString::map<float>
 
 inline float clamp(float value, float min, float max) {
 	return ((value < min) ? min : (value > max) ? max : value);
